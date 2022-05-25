82ebe6a265f87c7c08fcb39e63a9eba969eba524
roland <roland.lacap.270@my.csun.edu>
2022-05-25 16:31:37 -0700

    new new

--- drg_measurements_new/scripts/20220524_chilo_measurements.R
+++ drg_measurements_new/scripts/20220524_chilo_measurements.R
@@ -1,7 +1,7 @@
 ### Shark Soma size
 ### Created by Roland Lacap
 ### Created on 2022-01-07
-### Updated on 2022-03-18
+### Updated on 2022-05-24
 
 ###This is an updated version of a previous script containing the drg and tg
 ###measurements of bamboo shark and cat shark. This contains two different dev stages.
@@ -14,34 +14,26 @@ library(report)
 library(hablar)
 
 ### Load data
-master <- read_csv(here("drg_measurements_new","chilo","20220318_masterlist.csv"))
+master <- read_csv(here("drg_measurements_new","chilo","20220524_masterlist.csv"))
 
 ### statistics
-master <- master %>% 
+master <- master %>%
+  filter(exp != "tg 1 ngn1-594",
+         exp != "4 tg 26 3 20x 3",
+         exp != "4 tg 26 2 20x 1") %>% 
   mutate(avg_dia = x+y/2) %>%   #create average diameters between x and y
   filter(area <= 500,
          area >= 10) %>% 
-  unique() %>% 
-  select(1:5,9) %>% 
+  select(1:5,8,9) %>% 
   mutate(dev_stage = recode(dev_stage,
                            'Pre-hatch' = "Hatched",
                            '32' = "Stage 32")) %>% 
   mutate(dev_stage = factor(dev_stage,
                             levels = c("Stage 32",
-                                       "Hatched")))
-
-master2 <- master %>% 
-  filter(dev_stage == "Stage 32",
-         tissue == "TG") %>% 
-  mutate(avg_dia = sqrt(area)) %>% 
-  select(1:6)
-
-master3 <- master2 %>%
-  bind_rows(master) %>% 
-  na.omit()
+                                       "Hatched"))) %>% 
+  unique() #remove any duplicates
   
-
-duplicates <- master %>% 
+duplicates <- master %>% #finding any duplicates
   find_duplicates() %>% 
   unique()
   
@@ -78,7 +70,7 @@ area_p <- master %>%
         strip.text = element_text(size = 24))
 area_p
 
-snow_p <- master3 %>% 
+snow_p <- master %>% 
   gghistogram(x = "avg_dia", add = "mean", rug = TRUE,
               color = "tissue", fill = "tissue",
               bins = 40)+
---
+++ genome_alignments/scripts/20220525-genome-alignments.R
@@ -0,0 +1,107 @@
+### Genome alignments
+### Created by Roland Lacap
+### Created on 2022-05-25
+### Updated on 2022-05-25
+
+### This is primarily to show the NCBI alignments of nociceptive genes to 
+### elasmobranch genomes. Here, I show the percent identities.
+
+### Load libraries
+library(tidyverse)
+library(here)
+library(ggpubr)
+
+### Load data
+cp_align <- read_csv(here("genome_alignments","data","20220523-Cp.csv"))
+le_align <- read_csv(here("genome_alignments","data","20220523-Le.csv"))
+sc_align <- read_csv(here("genome_alignments","data","20220523-Sc.csv"))
+
+cpfilter <- cp_align %>% 
+  select(1,3:5) %>% 
+  group_by(Query) %>% 
+  filter(Perc_Identiy == max(Perc_Identiy)) %>%
+  filter(E_value == min(E_value)) %>% 
+  mutate(Gene = recode(Query,
+                      'Mus musculus neurotrophic tyrosine kinase, receptor, type 2 (Ntrk2), transcript variant X4, mRNA'="Ntrk2",
+                      'Mus musculus SRY (sex determining region Y)-box 10 (Sox10), mRNA'="Sox10",
+                      'Mus musculus ISL1 transcription factor, LIM/homeodomain (Isl1), mRNA'="Isl1",
+                      'Mus musculus neurofilament, medium polypeptide (Nefm), mRNA'="Nefm",
+                      'Mus musculus neurogenin 1 (Neurog1), mRNA'="Neurog1",
+                      'Mus musculus neurogenin 2 (Neurog2), mRNA'="Neurog2",
+                      'Mus musculus neurotrophic tyrosine kinase, receptor, type 1 (Ntrk1), transcript variant X1, mRNA'="Ntrk1",
+                      'Mus musculus neurotrophic tyrosine kinase, receptor, type 3 (Ntrk3), transcript variant X10, mRNA'="Ntrk3",
+                      'Mus musculus purinergic receptor P2X, ligand-gated ion channel, 3 (P2rx3), transcript variant X1, mRNA'="P2x3",
+                      'Mus musculus PR domain containing 12 (Prdm12), mRNA'="Prdm12",
+                      'Mus musculus ret proto-oncogene (Ret), transcript variant 4, mRNA'="Ret",
+                      'Mus musculus runt related transcription factor 1 (Runx1), transcript variant 3, mRNA'="Runx1",
+                      'Mus musculus runt related transcription factor 3 (Runx3), transcript variant X1, mRNA'="Runx3",
+                      'Mus musculus tachykinin 1 (Tac1), transcript variant X1, mRNA'="Tac1")) %>% 
+  mutate(name=c("Chiloscyllium punctatum")) %>% 
+  distinct()
+
+lefilter <- le_align %>% 
+  select(1,3:5) %>% 
+  group_by(Query) %>% 
+  filter(Perc_Identiy == max(Perc_Identiy)) %>% 
+  filter(E_value == min(E_value)) %>% 
+  mutate(Gene = recode(Query,
+                       'PREDICTED: Mus musculus neurotrophic tyrosine kinase, receptor, type 2 (Ntrk2), transcript variant X4, mRNA'="Ntrk2",
+                       'Mus musculus SRY (sex determining region Y)-box 10 (Sox10), mRNA'="Sox10",
+                       'Mus musculus ISL1 transcription factor, LIM/homeodomain (Isl1), mRNA'="Isl1",
+                       'Mus musculus neurofilament, medium polypeptide (Nefm), mRNA'="Nefm",
+                       'Mus musculus neurogenin 1 (Neurog1), mRNA'="Neurog1",
+                       'Mus musculus neurogenin 2 (Neurog2), mRNA'="Neurog2",
+                       'PREDICTED: Mus musculus neurotrophic tyrosine kinase, receptor, type 1 (Ntrk1), transcript variant X1, mRNA'="Ntrk1",
+                       'PREDICTED: Mus musculus neurotrophic tyrosine kinase, receptor, type 3 (Ntrk3), transcript variant X10, mRNA'="Ntrk3",
+                       'PREDICTED: Mus musculus purinergic receptor P2X, ligand-gated ion channel, 3 (P2rx3), transcript variant X1, mRNA'="P2x3",
+                       'Mus musculus PR domain containing 12 (Prdm12), mRNA'="Prdm12",
+                       'Mus musculus ret proto-oncogene (Ret), transcript variant 4, mRNA'="Ret",
+                       'Mus musculus runt related transcription factor 1 (Runx1), transcript variant 3, mRNA'="Runx1",
+                       'PREDICTED: Mus musculus runt related transcription factor 3 (Runx3), transcript variant X1, mRNA'="Runx3",
+                       'Mus musculus tachykinin 1 (Tac1), transcript variant X1, mRNA'="Tac1"))%>% 
+  mutate(name=c("Leucoraja erinacea")) %>% 
+  distinct()
+
+scfilter <- sc_align %>% 
+  select(1,3:5) %>% 
+  group_by(Query) %>% 
+  filter(Perc_Identiy == max(Perc_Identiy)) %>% 
+  filter(E_value == min(E_value)) %>% 
+  mutate(Gene = recode(Query,
+                       'Mus musculus neurotrophic tyrosine kinase, receptor, type 2 (Ntrk2), transcript variant X4, mRNA'="Ntrk2",
+                       'Mus musculus SRY (sex determining region Y)-box 10 (Sox10), mRNA'="Sox10",
+                       'Mus musculus ISL1 transcription factor, LIM/homeodomain (Isl1), mRNA'="Isl1",
+                       'Mus musculus neurofilament, medium polypeptide (Nefm), mRNA'="Nefm",
+                       'Mus musculus neurogenin 1 (Neurog1), mRNA'="Neurog1",
+                       'Mus musculus neurogenin 2 (Neurog2), mRNA'="Neurog2",
+                       'PREDICTED: Mus musculus neurotrophic tyrosine kinase, receptor, type 1 (Ntrk1)'="Ntrk1",
+                       'Mus musculus neurotrophic tyrosine kinase, receptor, type 3 (Ntrk3), transcript variant X10, mRNA'="Ntrk3",
+                       'Mus musculus purinergic receptor P2X, ligand-gated ion channel, 3 (P2rx3), transcript variant X1, mRNA'="P2x3",
+                       'Mus musculus PR domain containing 12 (Prdm12), mRNA'="Prdm12",
+                       'Mus musculus ret proto-oncogene (Ret), transcript variant 4, mRNA'="Ret",
+                       'Mus musculus runt related transcription factor 1 (Runx1), transcript variant 3, mRNA'="Runx1",
+                       'Mus musculus runt related transcription factor 3 (Runx3), transcript variant X1'="Runx3",
+                       'Mus musculus tachykinin 1 (Tac1), transcript variant X1, mRNA'="Tac1"))%>% 
+  mutate(name=c("Scyliorhinus canicula"))
+
+master <- cpfilter %>% 
+  full_join(lefilter) %>% 
+  full_join(scfilter) %>% 
+  mutate(Query_cover = as.numeric(parse_number(Query_cover))) 
+
+  
+# Plot
+
+p <- master %>% 
+  ggbarplot(x = "Gene", y = "Perc_Identiy", fill = "E_value") +
+  facet_wrap(~name)+
+  coord_flip()
+
+p
+
+pq <- master %>% 
+  ggbarplot(x = "Gene", y = "Perc_Identiy", fill = "Query_cover") +
+  facet_wrap(~name)+
+  coord_flip()
+
+pq
